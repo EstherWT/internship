8a22a1723a370306f4eff6f76ac9d27b1f3d6050
EstherWT <71790914+EstherWT@users.noreply.github.com>
2023-09-16 00:15:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -126,9 +126,6 @@ def update_internship():
     else:
         print("No rows were updated. It's possible that the intern_id does not exist.")
 
-    except mysql.connector.Error as err:
-        print("Error:", err)
-
     finally:
         cursor.close()
         db_conn.close()
