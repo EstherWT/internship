b1f21480061df488ec699222f384a9800ee542d3
EstherWT <71790914+EstherWT@users.noreply.github.com>
2023-09-16 17:17:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf'}
 app = Flask(__name__, static_folder='assets')
 #encrypt
 csrf = CSRFProtect(app)
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
 
 bucket = custombucket
 region = customregion
