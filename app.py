e64d767b2e6fcdb5fba7779ad0eb5dc21592020d
zihui <101386735+N0nEscapeSh3d0w@users.noreply.github.com>
2023-09-21 03:28:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -640,7 +640,7 @@ def applicationManagement():
     #not pending
     np_statement = "SELECT A.app_id, A.intern_id, A.stud_id, S.stud_name, I.job_title, A.status FROM Application A JOIN Student S ON A.stud_id = S.stud_id JOIN Internship I ON A.intern_id = I.intern_id WHERE A.status != %s AND A.company_id = %s"
     np_cursor = db_conn.cursor()
-    np_cursor.execute(statement, ("pending",com_id))
+    np_cursor.execute(np_statement, ("pending",com_id))
     not_pending_result = np_cursor.fetchall()
     
     return render_template('applicationManagement.html', pending=pending_result, not_pending = not_pending_result)
@@ -1135,6 +1135,140 @@ def viewReport(stud_id):
     result = cursor.fetchone() #Assuming there's only one student with the given ID
             
     return render_template('viewReport.html', report=result)
+
+#==Student========================================================================
+@app.route('/viewStudent/<string:stud_id>')
+def viewStudent(stud_id):
+
+    statement = "SELECT * FROM Student WHERE stud_id = %s"
+    cursor = db_conn.cursor()
+    cursor.execute(statement, (stud_id))
+    result = cursor.fetchone()
+
+    return render_template('student.html', student=result)
+
+@app.route('/studentEditPage/<string:stud_id>')
+def studentEditPage(stud_id):
+
+    statement = "SELECT * FROM Student WHERE stud_id = %s"
+    cursor = db_conn.cursor()
+    cursor.execute(statement, (stud_id))
+    result = cursor.fetchone()
+
+    return render_template('studentEdit.html', student=result)
+    
+@app.route('/updateStudent/<string:stud_id>', methods=['GET', 'POST'])
+@csrf.exempt 
+def update_Student(stud_id):
+
+    programme = request.form['programme']
+    student_group = request.form['grp']
+    cgpa = request.form['cgpa']
+    password = request.form['password']
+    intern_batch = request.form['intern_batch']
+    ownTransport = request.form['ownTransport']
+    currentAddress = request.form['currentAddress']
+    contactNo = request.form['contactNo']
+    personalEmail = request.form['personalEmail']
+    homeAddress = request.form['homeAddress']
+    homePhone = request.form['homePhone']
+    profile_img = request.files['profile_img']
+    resume = request.files['resume']
+
+    if profile_img.filename != "":
+        profile_img_in_s3 = "stud_id-" + str(stud_id) + "_png"
+        s3 = boto3.resource('s3')
+        s3.Bucket(custombucket).put_object(Key=profile_img_in_s3, Body=profile_img, ContentType=profile_img.content_type)
+        profile_img_url = f"https://{custombucket}.s3.amazonaws.com/{profile_img_in_s3}"
+         
+    if resume.filename != "":
+        resume_in_s3 = "stud_id-" + str(stud_id) + "_pdf"
+        s3 = boto3.resource('s3')
+        s3.Bucket(custombucket).put_object(Key=resume_in_s3, Body=resume, ContentType=resume.content_type)
+        resume_url = f"https://{custombucket}.s3.amazonaws.com/{resume_in_s3}"
+    
+    #no change in profile_img and resume
+    if profile_img.filename == "" and resume.filename == "":
+        statement = "UPDATE Student SET programme = %s, grp = %s, cgpa = %s, password = %s, intern_batch = %s, ownTransport = %s, currentAddress = %s, contactNo = %s, personalEmail = %s, homeAddress = %s, homePhone = %s WHERE stud_id = %s;"
+        cursor = db_conn.cursor()
+        cursor.execute(statement, (programme, student_group, cgpa, password, intern_batch, ownTransport, currentAddress, contactNo, personalEmail, homeAddress, homePhone, stud_id))
+        db_conn.commit()  # Commit the changes to the database
+    
+    #only change in resume
+    elif profile_img.filename == "" and resume.filename != "":
+        statement = "UPDATE Student SET programme = %s, grp = %s, cgpa = %s, password = %s, intern_batch = %s, ownTransport = %s, currentAddress = %s, contactNo = %s, personalEmail = %s, homeAddress = %s, homePhone = %s, resume = %s WHERE stud_id = %s;"
+        cursor = db_conn.cursor()
+        cursor.execute(statement, (programme, student_group, cgpa, password, intern_batch, ownTransport, currentAddress, contactNo, personalEmail, homeAddress, homePhone, resume_url, stud_id))
+        db_conn.commit()  # Commit the changes to the database 
+    
+        #only change in profile_img
+    elif profile_img.filename != "" and resume.filename == "":
+        statement = "UPDATE Student SET programme = %s, grp = %s, cgpa = %s, password = %s, intern_batch = %s, ownTransport = %s, currentAddress = %s, contactNo = %s, personalEmail = %s, homeAddress = %s, homePhone = %s, profile_img = %s WHERE stud_id = %s;"
+        cursor = db_conn.cursor()
+        cursor.execute(statement, (programme, student_group, cgpa, password, intern_batch, ownTransport, currentAddress, contactNo, personalEmail, homeAddress, homePhone, profile_img_url, stud_id))
+        db_conn.commit()  # Commit the changes to the database 
+    
+    #change both
+    else:
+        statement = "UPDATE Student SET programme = %s, grp = %s, cgpa = %s, password = %s, intern_batch = %s, ownTransport = %s, currentAddress = %s, contactNo = %s, personalEmail = %s, homeAddress = %s, homePhone = %s, profile_img = %s, resume = %s WHERE stud_id = %s;"
+        cursor = db_conn.cursor()
+        cursor.execute(statement, (programme, student_group, cgpa, password, intern_batch, ownTransport, currentAddress, contactNo, personalEmail, homeAddress, homePhone, resume_url, profile_img_url, stud_id))
+        db_conn.commit()  # Commit the changes to the database 
+    
+    return redirect('/viewStudent/' + stud_id)
+
+@app.route('/submitReport/<string:stud_id>', methods=['GET', 'POST'])
+@csrf.exempt 
+def submit_Report(stud_id):
+
+    #Get last ID
+    countstatement = "SELECT report_id FROM Report ORDER BY report_id DESC LIMIT 1;"
+    count_cursor = db_conn.cursor()
+    count_cursor.execute(countstatement)
+    result = count_cursor.fetchone()
+    
+    if result is None or result[0] is None:
+        report_id = 1
+    else:
+        report_id = int(result[0]) + 1
+    
+    count_cursor.close()
+
+
+    report_title = request.form['report_title']
+    report_type = request.form['report_type']
+    report = request.files['report']
+    
+    if report.filename != "":
+        # Check if a file was uploaded
+        if 'report' in request.files:
+            report = request.files['report']
+        
+            # Check if the uploaded file is allowed
+            if report and allowed_file(report.filename):
+                cursor = db_conn.cursor()
+                report_in_s3 = "report_id-" + str(report_id) + "_pdf"
+                s3 = boto3.resource('s3')
+        
+                try:
+                    print("Data inserted in MySQL RDS... uploading pdf to S3...")
+                    s3.Bucket(custombucket).put_object(Key=report_in_s3, Body=report, ContentType=report.content_type)
+            
+                   # Generate the object URL
+                    report_url = f"https://{custombucket}.s3.amazonaws.com/{report_in_s3}"
+                    insert_sql = "INSERT INTO Report VALUES (%s, %s, %s, %s, %s)"
+                    cursor.execute(insert_sql, (report_id, stud_id, report_title, report_type, report_url))
+                    db_conn.commit()  # Commit the changes to the database
+                    
+                    return redirect('/SupervisorStudPage/' + stud_id)
+                except Exception as e:
+                    return str(e)
+                finally:
+                    cursor.close()
+            else:
+              return "Invalid file format. Allowed formats are: " + ", ".join(ALLOWED_EXTENSIONS)
+        
+    return "No file uploaded."
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
